e92ef1f4938694049af13b10d25908cc66f2b262
techsudhagar <techsudhagar@gmail.com>
2021-02-06 09:08:36 -0600

    Assistant calls reduced

--- gmail/quickstart/gmail.js
+++ gmail/quickstart/gmail.js
@@ -1,19 +1,4 @@
-/**
- * @license
- * Copyright Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 // [START gmail_quickstart]
 const fs = require('fs');
 const readline = require('readline');
@@ -117,23 +102,7 @@ function listLabels(auth) {
     }
   });
 }
-/*
-function watchInbox(auth) {
-const gmail = google.gmail({ version: 'v1', auth });
-const respose = gmail.users.watch({
-  userId: 'techsudhagar@gmail.com',
-  requestBody: {
-    // Replace with `projects/${PROJECT_ID}/topics/${TOPIC_NAME}`
-    //topicName: 'projects/sun-smart-home-301512/topics/SensorNotification'
-    topicName: 'projects/sun-smart-home-301512/topics/SensorNotification',
-  },
-});
 
-console.log(` Gmail Watch Status..: ${respose.data}`);
-
-}
-
-*/
 
 function listEmails(auth) {
 
@@ -226,7 +195,7 @@ function listEmails(auth) {
 
             if (is_motion_detected && kids_light_sensor) {
 
-              assistant.changeLightState('Tube Light', 'On');
+              assistant.changeLightState('Tube Light', 'Turn On');
             }
 
             is_motion_detected = false;
--- modules/assistant/commandAssistant.js
+++ modules/assistant/commandAssistant.js
@@ -1,104 +1,74 @@
 
 const http = require('http');
-
+const DEVICE_TYPE_CAMERA = 'Camera';
+const DEVICE_TYPE_LIGHT = 'Light';
+const TURN_ON = 'Turn On';
+const SHOW = 'Show';
 
 function showCamera(which_camera) {
 	console.log(` ${which_camera} camera would be streamed`);
 
-	const command  = `{
-		"command": " Show ${which_camera} camera on Hub",
-		"converse": false,
-		"user": "techsudhagar@gmail.com" 
-		}`;
-
-
-	commandAssistant(command);
+	const command = getPassthruCommand(which_camera,DEVICE_TYPE_CAMERA,SHOW);
+	commandPassthruAssistant(command);
 
 }
 
-function turnLightState(state) {
-	console.log(` Turning light state to ${state}.`);
+function changeLightState(device_name, state) {
+	console.log(` ${device_name} ${state} `);
 
-	const command  = `{
-		"command": "Turn ${state} tube light",
-		"converse": false,
-		"user": "techsudhagar@gmail.com" 
-		}`;
+	const command = getPassthruCommand(device_name,DEVICE_TYPE_LIGHT,state);
+	commandPassthruAssistant(command);
 
-
-	commandAssistant(command);
 
 }
 
-function changeLightState(device_name,state) {
-	console.log(` Turning ${device_name} to ${state}.`);
+function commandPassthruAssistant(assistant_request) {
 
-	const command  = `{
-		"command": "Turn ${state} ${device_name}",
-		"converse": false,
-		"user": "techsudhagar@gmail.com" 
-		}`;
-
-
-	commandAssistant(command);
-
-}
-
-
-function notifyAssistant(event_title,diff_mins){
-	console.log(`Event Title..:${event_title},..:${diff_mins}:`);
-
-				console.log("Event announcement..");
-
-				commandAssistant(getBroadcastCommand(event_title,diff_mins));
-			
-
-	}
-
-	function getBroadcastCommand(event_title, in_mins) {
-
-		return `{
-		  "command": " You have ${event_title} event starting in ${in_mins} minutes",
-		  "broadcast": true,
-		  "user": "techsudhagar@gmail.com" 
-		  }`;
-	  
-	  
-	  }
-
-
-	function commandAssistant(assistant_request) {
-
-		//console.log(`assistant_request..: ${assistant_request}`)
-		console.info(`Assistant Command Executing..:${assistant_request}`);
-		const options = {
-		  hostname: 'localhost',
-		  port: 3000,
-		  path: '/assistant',
-		  method: 'POST',
-		  headers: {
+	//console.log(`assistant_request..: ${assistant_request}`)
+	console.info(`Assistant Passthru Command Executing..:${assistant_request}`);
+	const options = {
+		hostname: 'localhost',
+		port: 5000,
+		path: '/assistant/passthru/command',
+		method: 'POST',
+		headers: {
 			'Content-Type': 'application/json',
 			'Content-Length': assistant_request.length
-		  }
 		}
-	  
-		const req = http.request(options, res => {
-		  console.log(`statusCode: ${res.statusCode}`)
-	  
-		  res.on('data', d => {
-			process.stdout.write(d)
-		  })
-		})
-	  
-		req.on('error', error => {
-		  console.error(error)
-		})
-	  
-		req.write(assistant_request)
-		req.end()
-	  
-		console.info(`Assistant Command Executed`);
-	  
-	  }
+	}
 
-    module.exports = { notifyAssistant, showCamera,turnLightState,changeLightState};
+	const req = http.request(options, res => {
+		console.log(`statusCode: ${res.statusCode}`)
+
+		res.on('data', d => {
+			process.stdout.write(d)
+		})
+	})
+
+	req.on('error', error => {
+		console.error(error)
+	})
+
+	req.write(assistant_request)
+	req.end()
+
+	console.info(`Assistant Passthru Command Executed`);
+
+}
+
+
+function getPassthruCommand(device_name, device_type, action) {
+
+	
+
+	var command_request = `{
+		  "device_name": "${device_name}",
+		  "action": "${action}",
+		  "device_type": "${device_type}" 
+	  }`;
+
+	return command_request;
+
+}
+
+module.exports = { showCamera, changeLightState };
